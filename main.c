61e26a85bdfef6e52ae91d4611382154dcb7a424
Mateus Emanuel <Mateusemanuelcardoso@gmail.com>
2025-04-13 09:43:32 -0300

    Update main.c
    Adicionadas as funções que cada um deve escolher no projeto.

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+//Fazer um sistema capaz de cadastrar, registrar e organizar elementos digitados pelo usuário.
+// - Nome, CPF e Altura.
+//Nome [200]: Apenas receber.
+//CPF[11 digitos]: Deve ser feita uma verificação no próprio sistema para ver se o CPF está válido.
+//Se inválido, pedir para digitar denovo.
+//Altura [0,5 a 3,00(em metros)]: Ao passar disso, pedir para digitar denovo.
+//-  Ideia Inicial: Ao fim da digitação do usuário, o sistema deve imprimir na tela o que o usuário
+//digitou (No caso de informações válidas).
 
+//Pessoa 1: Responsável por criar o esqueleto do programa. Vai implementar os comandos de entrada e saída,
+//como scanf e printf, para receber as informações dos usuários.
+
+//Pessoa 2: Fica com a parte da validação do CPF. Implementar o cálculo que confirma se o CPF digitado é
+//válido. Se o CPF estiver incorreto, o programa deve solicitar que o usuário digite novamente.
+
+// - Pessoa 3: Vai desenvolver a função de validação do nome. Se forem detectados números no nome, o
+//programa deve pedir uma nova entrada até que o nome esteja correto.
+
+//Pessoa 4: Responsável pela validação da altura. Se a altura informada ultrapassar o limite máximo
+//permitido, o programa precisa requisitar uma nova leitura até que o valor esteja correto pelo parâmetro
+//que foi estabelecido.
